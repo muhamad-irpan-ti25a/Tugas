05eae42b8c3bb0b83ee05c9f16dd0bf19b4143e4
Muhamad Irpan <muhamad.irpan_ti25@nusaputra.ac.id>
2026-02-27 11:17:33 +0700

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,9 @@
+angka1 = int(input("angka ke 1 :"))
+angka2 = int(input("angka ke 2 :"))
+
+if angka1 > angka2 :
+    terbesar = angka1
+else : 
+    terbesar = angka2
+
+print("angka terbesar adalah :", terbesar)
